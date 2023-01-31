2a14a489d1cfb92b9b912bbe913686cb9967b9bf
Frank Dillon <dillonf2@outlook.com>
2023-01-30 22:22:48 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+let names=[`Guadalupe`, `Ollie`,`Aki`]
+
+function writeCards(names) {
+    let cards=[]
+  for (let i = 0; i < names.length; i++) {
+    cards[i]=`Thank you, ${names[i]}, for the wonderful surprise gift!`
+  }
+  return cards
+}
+writeCards(names)
+
+function countDown(n) {
+    for (let i =n; i >= 0; i--){
+        console.log(i)
+    }
+}
